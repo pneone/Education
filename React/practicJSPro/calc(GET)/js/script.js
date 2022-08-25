15776e63e29e91c579684f2d170f852cdf1d031e
Pavel <obermkozno@gmail.com>
2022-08-25 17:10:39 +0300

    GET POST

---
+++ React/practicJSPro/calc(GET)/js/script.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const inputUAH = document.querySelector('#uah'),
+      inputUSD = document.querySelector('#usd');
+
+inputUAH.addEventListener('input', () =>{
+    const request = new XMLHttpRequest();
+
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+
+    // request.addEventListener('readystatechange', () =>{
+    //     if(request.readyState === 4 && request.status === 200){
+    //         console.log(request.response);
+
+    //         const data = JSON.parse(request.response);
+
+    //         inputUSD.value = (+inputUAH.value / data.current.usd).toFixed(2);
+    //     }else{
+    //         inputUSD.value = "err";
+    //     }
+    // });
+
+    request.addEventListener('load', () =>{
+        if(request.status === 200){
+            console.log(request.response);
+
+            const data = JSON.parse(request.response);
+
+            inputUSD.value = (+inputUAH.value / data.current.usd).toFixed(2);
+        }else{
+            inputUSD.value = "err";
+        }
+    });
+
+    // status
+    // statusText
+    // response
+    // readyState
+
+
+
+});
